052b71e5f86a229d1a976fa54f9885ef37caa855
Rafael Dalago Algauer <rdalago@gmail.com>
2026-02-10 17:36:06 -0300

    Configure gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/rafaeldalago.github.io/",
+  base: "/terroir-glass/",
   plugins: [react()],
 });
